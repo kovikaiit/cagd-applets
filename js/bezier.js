18ca5ef1977d2ff849a73eba5384b3bbefa7c4a9
kovikaiit <pisti_macbook@Istvan-MacBook-Pro.local>
2016-04-03 18:17:22 +0200

    BezierCurve
    Simple Bezier curve and js files.

---
+++ js/bezier.js
@@ -0,0 +1,64 @@
+
+
+function BernsteinBase(i, n, u)
+{
+	var temp = new Array();
+	for (k = 0; k <= n; k++) 
+	{
+	    temp.push(0.0);
+	}
+	temp[n - i] = 1.0;
+	var u1 = 1.0 - u;
+	for  (k = 1; k <= n; k++) 
+	{
+	   for (j = n; j>=k; j--) 
+	   {
+	       temp[j] = u1 * temp[j] + u * temp[j-1];
+	   }
+	}
+	return temp[n];
+}
+
+function AllBernstein(n, u) 
+{
+    var B = new Array();
+	for (k = 0; k <= n; k++) 
+	{
+	    B.push(0.0);
+	}
+    B[0] = 1.0;
+    var u1 = 1.0 - u;
+    for (j = 1; j <= n; j++) {
+        var saved = 0.0;
+        for (k = 0; k < j; k++) {
+            var temp = B[k];
+            B[k] = saved + u1 * temp;
+            saved = u * temp;
+        }
+        B[j] = saved;
+    }
+    return B;
+}
+
+
+function PointOnBezierCurve(P, n, u)
+{ 
+	B = AllBernstein(n, u);
+	var C = new THREE.Vector3(0.0, 0.0, 0.0);
+	for (k = 0; k <= n; k++) 
+	{
+		C.addScaledVector(P[k], B[k]);
+	}
+	return C;
+}
+
+var BezierCurve = THREE.Curve.create(
+    function ( P, n ) { //custom curve constructor
+        this.P = P;
+		this.n = n
+    },
+
+    function ( t ) { //getPoint: t is between 0-1
+        return PointOnBezierCurve(this.P, this.n, t);
+    }
+);
